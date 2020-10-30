becc28b4ef7506233533917341a9a5216b17428e
aidazh <zhoroeva.aida@gmail.com>
2020-10-30 20:04:58 +0600

    First tasks

---
+++ aidatask5.py
@@ -0,0 +1,22 @@
+# a = 'hello'
+# b = 'world,'
+# c = "I'm"
+# d = 'John!'
+# f = a + " " + b + " " + c + " " + d 
+# print(f)
+
+# a = "hello "
+# b = a * 3
+# print(b)
+
+# a = input('Write your name: ')
+# b = input('Write your surname: ')
+# c = ('Hello, ' + a +' ' + b + '!')
+# print(c)
+
+# user = input('Write any word: ')
+# srez = user[ : :-1]
+# print(srez)
+
+
+
---
+++ taskaida.py
@@ -0,0 +1,22 @@
+# a = 5   
+# b = 10
+# c = 2
+# print((a+b)*c)
+
+# a = 10    
+# b = 3
+# print(a%b)
+
+# user_age = input('You are age: ')  
+# print('You are age: ' + user_age)
+
+user1 = 10
+user2 = 2
+# user1 += user2
+# print(user1)
+
+#print(user1-user2)
+
+#print(user1 * user2)
+
+print(user1 // user2)
